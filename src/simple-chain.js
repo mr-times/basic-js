05e1becfa9fab276990fe8569c5632652680c592
Pavel Savelyev <mr_times@mail.ru>
2023-12-25 05:38:21 +0700

    feat: all done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+
+/*
+class Item {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+*/
+
 const chainMaker = {
+  array: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value = '') {
+    if (value !== undefined) {this.array.push(`( ${value} )`)};
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (position > this.array.length || position < 1 || position !== parseInt(position)) {
+      this.array = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.array.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const str = this.array.join('~~');
+    this.array = [];
+    return str;
   }
 };
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  constructor(a = true) {
+    this.direct = a;
+}
+
+checkArguments(text, key) {
+    if (text === undefined || key === undefined) throw new Error('Incorrect arguments!');
+    else {
+        key = key.toUpperCase();
+        text = text.toUpperCase();
+    };
+}
+
+encrypt(text, key) {
+    this.checkArguments(text, key);
+    console.log(text);
+    console.log(key);
+    if (key.length < text.length) {
+        key = key.repeat(Math.ceil(text.length / key.length));
+    }
+    key = key.toUpperCase();
+    text = text.toUpperCase();
+    console.log(text);
+    console.log(key);
+    let codedText = '';
+    let j = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].match(/[a-zA-Z]/)) {
+            console.log(`Match ${text[i]}`);
+            console.log(text.charCodeAt(i));
+            console.log(`Match ${key[j]}`);
+            console.log(key.charCodeAt(i));
+            codedText += String.fromCharCode(65 + (text.charCodeAt(i) - 65 + key.charCodeAt(j) - 65) % 26);
+            j++;
+
+            console.log(`calculated ${65 + (text.charCodeAt(i) - 65 + key.charCodeAt(i) - 65) % 26}`);
+        } else {
+            console.log(`Not Matched ${text[i]}`);
+            codedText += text[i];
+        }
+    }
+
+    if (this.direct) {
+        return codedText
+    } else { return codedText.split("").reverse().join("") };
+
+}
+decrypt(text, key) {
+    this.checkArguments(text, key);
+
+    console.log(text);
+    console.log(key);
+
+    if (key.length < text.length) {
+        key = key.repeat(Math.ceil(text.length / key.length));
+    }
+    key = key.toUpperCase();
+    text = text.toUpperCase();
+
+    console.log(text);
+    console.log(key);
+
+    let codedText = '';
+    let j = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].match(/[a-zA-Z]/)) {
+            console.log(`Match ${text[i]}`);
+            console.log(text.charCodeAt(i));
+            console.log(`Match ${key[j]}`);
+            console.log(key.charCodeAt(i));
+            codedText += String.fromCharCode(65 + (text.charCodeAt(i) - key.charCodeAt(j) + 26) % 26);
+            j++;
+
+            console.log(`calculated ${(65 + (text.charCodeAt(i) - key.charCodeAt(j) + 26) % 26)}: ${String.fromCharCode(65 + (text.charCodeAt(i) - key.charCodeAt(j) + 26) % 26)}`);
+        } else {
+            console.log(`Not Matched ${text[i]}`);
+            codedText += text[i];
+        }
+    }
+
+
+    if (this.direct) {
+        return codedText
+    } else { return codedText.split("").reverse().join("") };
+}
 }
 
 module.exports = {
